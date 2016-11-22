92d523417a7db016cca98460761a97271ae841f8
Conor Campbell <campbellcj11@gmail.com>
2016-11-22 10:54:49 -0500

    Changed Event actions to have a function that gets the saved events of the current user according to the current database model schema.

--- app/actions/eventActions.js
+++ app/actions/eventActions.js
@@ -8,32 +8,21 @@ var {
   Platform,
 } = ReactNative;
 
-//reducers
-
-//initialize firebase TODO:pull from a credentials file
-const firebaseConfig = {
-  apiKey: "AIzaSyBc6_49WEUZLKCBoR8FFIHAfVjrZasdHlc",
-  authDomain: "projectnow-964ba.firebaseapp.com",
-  databaseURL: "https://projectnow-964ba.firebaseio.com",
-  storageBucket: "projectnow-964ba.appspot.com",
-  messagingSenderId: "14798821887"
-};
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-//initialize database
 const database = firebase.database();
 
-//not tested TODO:add state for this
-export function pushEvent(event) {
-  database.ref('events/').push({
-    title: event.title,
-    shortDescription: event.shortDescription,
-    longDescription: event.longDescription,
-    photo: event.photo, //TODO:pull from storage
-    video: event.video, //TODO:pull from storage
-    startDate: event.startDate,
-    endDate: event.endDate,
+export function getSavedEvents()
+{
+  var query = database.ref("savedEvents/" + firebase.auth().currentUser.uid).orderByKey();
+  var index = 0;
+  var savedEventKeys = [];
+  query.once("value")
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key;
+        console.log("KEY: " + key + " Index: " + index);
+        savedEventKeys[index] = key;
+        index++;
+    });
+    return savedEventKeys;
   });
-  return {
-    //add reducer components here
-  }
 }
