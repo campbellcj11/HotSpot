6aced74af39bd150aeb6a505c9fbd6d982a19189
Daniel Clements <clem.daniel@gmail.com>
2017-04-05 02:33:13 -0400

    working rapid search functionality for current locale on discover page using case insensitive regular expressions

--- app/Utils/styleVariables.js
+++ app/Utils/styleVariables.js
@@ -8,6 +8,7 @@ var styleVariables = {
   systemRegularFont: Platform.OS == 'ios' ? 'Helvetica Neue' : 'sans-serif',
   systemLightFont: Platform.OS == 'ios' ? 'HelveticaNeue-Light' : 'sans-serif-light',
   greyColor: '#A6A6A6',
+  titleBarHeight: Platform.OS == 'ios' ? 64 : 54
 }
 
 export default styleVariables
